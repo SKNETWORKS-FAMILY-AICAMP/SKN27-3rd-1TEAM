0dc79a0c05d67d21828d2d17e8d9b0bd6ba3ac4f
hjoo10200 <hjoo10200@gmail.com>
2026-05-15 02:23:57 +0900

    분석/계산 에이전트 최적화 진행 및 미니게임 일부 수정

--- common/get_model.py
+++ common/get_model.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_groq import ChatGroq  # noqa: E402
+from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings  # noqa: E402
 
 
@@ -13,9 +14,8 @@ def get_embedding_model() -> OpenAIEmbeddings:
     return emb
 
 
-def get_llm() -> ChatGroq:
-    llm = ChatGroq(
-        model="openai/gpt-oss-120b",
-        temperature=0.2,
+def get_llm() -> ChatOpenAI:
+    llm = ChatOpenAI(
+        model="gpt-5.4-nano"
     )
     return llm
